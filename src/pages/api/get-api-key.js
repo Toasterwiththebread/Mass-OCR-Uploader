7fed3f303907536e0987a3a206b138e1e4de9e4a
Toaster <87452263+Toasterwiththebread@users.noreply.github.com>
2024-03-19 10:57:25 -0600

    Update get-api-key.js

--- src/pages/api/get-api-key.js
+++ src/pages/api/get-api-key.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-	if (process.env[".OCR_SPACE_API_KEY"]) {
-		res.status(200).json({ key: process.env[".OCR_SPACE_API_KEY"] });
+	if (process.env.OCR_SPACE_API_KEY) {
+		res.status(200).json({ key: process.env.OCR_SPACE_API_KEY });
 	} else {
 		res.status(500).json({ error: "No API key found" });
 	}
